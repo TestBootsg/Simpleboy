274689123ba2971ecfb0ff475d651ce4c15eb632
My Official Account <106927029+My-Official0698@users.noreply.github.com>
2022-08-24 22:01:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -52,7 +52,7 @@ class Bot(Client):
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/CodeXBotz")
+        self.LOGGER(__name__).info(f"Bot Started....!")
         self.username = usr_bot_me.username
 
     async def stop(self, *args):
